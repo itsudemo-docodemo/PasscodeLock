50daab914f2dff4d11a9865d77dd5e3a585948c0
itsudemo-docodemo <itsudemo.docodemo@gmail.com>
2022-10-06 14:16:45 +0900

    test commit

--- PasscodeLock/SceneDelegate.swift
+++ PasscodeLock/SceneDelegate.swift
@@ -45,6 +45,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        print("applicationDidEnterBackground: バックグラウンドへ移行完了した時")
+        // パスコードロック画面を表示する
+//        displayPasscodeLockScreenIfNeeded()
+
     }
 
 
